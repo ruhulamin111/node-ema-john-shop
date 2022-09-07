307c9cad1fe91b3e3dcd602b8f0c73235568519d
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-07 12:32:47 +0600

    skip add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -17,11 +17,33 @@ async function run() {
         const productCollection = client.db("shop").collection("product");
 
         app.get('/product', async (req, res) => {
+            console.log(req.query);
             const query = {}
+            const page = parseInt(req.query.page)
+            const size = parseInt(req.query.size)
             const cursor = productCollection.find(query)
-            const result = await cursor.toArray()
+            let result;
+            if (page || size) {
+                result = await cursor.skip(page * size).limit(size).toArray()
+            }
+            else {
+                result = await cursor.toArray()
+            }
             res.send(result)
         })
+
+        app.get('/productcount', async (req, res) => {
+            // const query = {}
+            // const cursor = productCollection.find(query)
+            const countPage = await productCollection.countDocuments();
+            res.send({ countPage })
+        })
+
+        // app.post('/product', async (req, res) => {
+        //     const query = { name: 'abc' }
+        //     const result = productCollection.insertOne(query)
+        //     res.send(result)
+        // })
 
 
     }
