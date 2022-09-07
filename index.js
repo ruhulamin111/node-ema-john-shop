d7690e580c3ada1895a3b46e4f66025b8a1d47c1
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-07 06:34:31 +0600

    mongodb add

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -13,3 +14,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('working port', port);
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const productCollection = client.db('shop').collection('product')
+        console.log('db connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
