4f6d4897dbc0ba26164bf9963b651db011be5609
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-07 06:26:54 +0600

    node server setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('ema jhon shop')
+})
+app.listen(port, () => {
+    console.log('working port', port);
+})
